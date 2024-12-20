8c505231a9d10a5b1bf9e25f0783c14a7efb2c56
labh014 <labhverma551@gmail.com>
2024-12-20 22:26:04 +0530

    add dirname final

--- backend/src/app.js
+++ backend/src/app.js
@@ -24,13 +24,13 @@ app.use(express.json({limit: "40kb"}));
 app.use(express.urlencoded({limit: "40kb", extended: true}));
 
 app.use("/api/v1/users", userRoutes);
-app.use(express.static(path.join(__dirname, 'frontend/dist')));
+// Serve the React build files from frontend/dist
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
 
-// Serve the React frontend for non-API routes
+// Catch-all route to serve index.html for any route
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve('frontend/dist/index.html'));
+  res.sendFile(path.resolve(__dirname, '..', 'frontend', 'dist', 'index.html'));
 });
-
 
 app.get("/home" , (req, res) => {
   return res.json({"Hello" : "world"});
