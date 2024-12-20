0a0344f8fed0c81d0541a4d7c5a40e7a1826206e
labh014 <labhverma551@gmail.com>
2024-12-20 22:10:27 +0530

    add path and viteconfig.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,8 @@ import { create } from "node:domain";
 import { connectToSocket } from "./controller/serverController.js";
 import userRoutes from "./routes/userRoutes.js"
 import { checkTokenExpiry } from "./middlewares/tokenCheck.js";
+import path from 'path';
+
 
 
 const app = express();
@@ -17,10 +19,16 @@ app.set("port", process.env.PORT || 8080);
 app.use(cors({
     origin: 'https://livehorizonfrontend.onrender.com'
 }));
+
 app.use(express.json({limit: "40kb"}));
 app.use(express.urlencoded({limit: "40kb", extended: true}));
 
 app.use("/api/v1/users", userRoutes);
+
+// Serve the React frontend for non-API routes
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('frontend/dist/index.html'));
+});
 
 
 app.get("/home" , (req, res) => {
