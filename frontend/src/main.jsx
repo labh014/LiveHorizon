a1c9a6796e7726254d27d05f97814d55fea87dc4
labh014 <labhverma551@gmail.com>
2024-12-20 21:25:34 +0530

    revert BrowserRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from "react-router-dom";
-import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <StrictMode>
     <App />
-  </BrowserRouter>,
+  </StrictMode>,
 )
