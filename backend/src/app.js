484bf636a818285aabd6b1aa9f19b4734be75984
labh014 <labhverma551@gmail.com>
2024-12-20 22:29:00 +0530

    revert add dirname

--- backend/src/app.js
+++ backend/src/app.js
@@ -24,7 +24,6 @@ app.use(express.json({limit: "40kb"}));
 app.use(express.urlencoded({limit: "40kb", extended: true}));
 
 app.use("/api/v1/users", userRoutes);
-app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
 // Serve the React frontend for non-API routes
 app.get('*', (req, res) => {
