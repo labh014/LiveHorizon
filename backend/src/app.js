c49dd7bba1836d38f197b8e5c86744a8338f2de0
labh014 <labhverma551@gmail.com>
2025-01-08 16:35:45 +0530

    add env

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,13 +7,20 @@ import { create } from "node:domain";
 import { connectToSocket } from "./controller/serverController.js";
 import userRoutes from "./routes/userRoutes.js"
 import { checkTokenExpiry } from "./middlewares/tokenCheck.js";
+import { fileURLToPath } from "node:url";
 import path from 'path';
+import 'dotenv/config'; 
+const PORT = process.env.PORT || 8080;
 
 
 
 const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 app.set("port", process.env.PORT || 8080);
 app.use(cors({
@@ -53,12 +60,12 @@ app.get("/home" , (req, res) => {
 })
 
 const main = async () => {
-  const db = await mongoose.connect("mongodb+srv://labh:KgH3wPZt4lSiDGmR@cluster0.3rdzb.mongodb.net/Cluster0")
+  const db = await mongoose.connect(process.env.DATABASE_URL)
   console.log("conection to database is succesfully")
 }
 main();
 
-server.listen(8080, () => {
-  console.log("Listening on port 8080");
+server.listen(PORT, () => {
+  console.log(`Listening on port 8080 ${PORT}`);
 })
 
