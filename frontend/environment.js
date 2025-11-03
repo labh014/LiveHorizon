bdc83bc100179d3c15872562744f7b08646247d0
labh014 <labhverma551@gmail.com>
2025-11-03 23:51:07 +0530

    change env.js

--- frontend/environment.js
+++ frontend/environment.js
@@ -1,4 +1,6 @@
 let production = true;
 
-const server = production ? "https://livehorizon.onrender.com" : "http://localhost:8080";
+// const server = production ? "https://livehorizon.onrender.com" : "http://localhost:8080";
+const server = production ? "http://ec2-3-110-176-116.ap-south-1.compute.amazonaws.com:8080" : "http://localhost:8080";
 export default server
+
